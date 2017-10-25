edfe977bf28e0615d1ef405c1c7437e370034dd5
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2017-10-25 17:42:45 +0300

    knowledge pages

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
+	// header city dropdown
+	$('.js-cityDropdown').click(function(e){
+		e.preventDefault();
+
+		$(this).toggleClass('active');
+		$('.js-cityList').toggleClass('active');
+	});
 });
